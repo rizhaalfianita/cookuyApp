02d7e43fbec18f2d79230c89d69e48eac0b1e7cc
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2023-03-02 13:26:35 +0700

    Update resume recipe

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,22 @@ import 'package:cookuy/views/home.dart';
 import 'package:cookuy/views/saved.dart';
 import 'package:flutter/material.dart';
 
+import 'views/ResumeIngredient.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  MyApp({super.key});
+  List meals = [];
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Nunito"),
-      home: RecomRecipe(),
+      home: ResumeIngredient(meals: meals),
     );
   }
 }
